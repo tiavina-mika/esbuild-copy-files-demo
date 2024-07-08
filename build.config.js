6aa95961973abdac2a63b5559159f5857b0aed06
tiavina-mika <tiavinamika@gmail.com>
2024-07-08 10:46:10 +0300

    Update build.config.js

--- build.config.js
+++ build.config.js
@@ -1,7 +1,8 @@
 const esbuild = require('esbuild');
 const path = require('path');
 const { nodeExternalsPlugin } = require("esbuild-node-externals");
-// const copy = require("esbuild-copy-files");
+const { copy } = require("esbuild-copy-files");
+console.log('copy: ', copy);
 
 const buildOptions = {
   entryPoints: [
@@ -13,21 +14,21 @@ const buildOptions = {
   outdir: 'dist',
   plugins: [
     nodeExternalsPlugin(),
-    // copy({
-    //   assets: [
-    //     {
-    //       from: ['./src/folder1/subfolder1'],
-    //       to: ['./dist/folder1/subfolder1'],
-    //       ignoreFiles: ['*1.json'],
-    //       watch: true,
-    //     },
-    //     {
-    //       from: ['./src/folder2/subfolder2'],
-    //       to: ['./dist/folder1/subfolder2'],
-    //       watch: false,
-    //     },
-    //   ]
-    // })
+    copy({
+      assets: [
+        {
+          from: ['./src/folder1/subfolder1'],
+          to: ['./dist/folder1/subfolder1'],
+          ignoreFiles: ['*1.json'],
+          watch: true,
+        },
+        // {
+        //   from: ['./src/folder2/subfolder2'],
+        //   to: ['./dist/folder1/subfolder2'],
+        //   watch: false,
+        // },
+      ]
+    })
   ],
 };
 
