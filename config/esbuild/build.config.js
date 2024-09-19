df58bca0043f9583b1acb45c8346114d8531755d
tiavina-mika <tiavinamika@gmail.com>
2024-09-19 20:19:36 +0300

    Update build.config.js

--- config/esbuild/build.config.js
+++ config/esbuild/build.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { nodeExternalsPlugin } = require("esbuild-node-externals");
 const { copy } = require("esbuild-copy-files");
 
-const buildOptions = {
+const defaultBuildOptions = {
   entryPoints: [
     { in: path.resolve(__dirname, '../../src/index.ts'), out: path.resolve(__dirname, '../../dist/index') },
   ],
@@ -11,6 +11,42 @@ const buildOptions = {
   bundle: true,
   platform: 'node',
   outdir: 'dist',
+};
+
+/**
+ * options for building the project
+ * see the script 'build' in package.json
+ * we are using the watch option to watch for changes
+ */
+const buildOptions = {
+  ...defaultBuildOptions,
+  plugins: [
+    nodeExternalsPlugin(),
+    copy({
+      // stop watching for build
+      stopWatching: true,
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../../src/folder2'),
+          to: path.resolve(__dirname, '../../dist/folder2'),
+          ignore: ['*3.json'],
+        },
+        // {
+        //   from: ['./src/folder2/subfolder2'],
+        //   to: ['./dist/folder1/subfolder2'],
+        //   watch: false,
+        // },
+      ]
+    }),
+  ],
+}
+
+/**
+ * options for building the project in watch mode (watching for changes)
+ * we do not stop watching for changes (stopWatching: false)
+ */
+const watchBuildOptions = {
+  ...defaultBuildOptions,
   plugins: [
     nodeExternalsPlugin(),
     copy({
@@ -22,19 +58,12 @@ const buildOptions = {
           ignore: ['*3.json'],
           watch: true,
         },
-        // {
-        //   from: ['./src/folder2/subfolder2'],
-        //   to: ['./dist/folder1/subfolder2'],
-        //   watch: false,
-        // },
       ]
     }),
   ],
-};
+}
 
-
-
-exports.buildOptions = buildOptions;
+exports.watchBuildOptions = watchBuildOptions;
 
 async function build() {
   await esbuild.build(buildOptions);
